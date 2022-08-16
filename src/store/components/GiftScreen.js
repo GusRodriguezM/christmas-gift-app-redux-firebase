496ea22ac7f985aaf370ede8ef93e5a597547ba2
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-08-15 23:35:03 -0500

    Step 2: Adding basic styles

--- src/store/components/GiftScreen.js
+++ src/store/components/GiftScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import './styles.css';
+
 export const GiftScreen = () => {
 
     const { gifts } = useSelector( state => state.gifts );
 
     return (
-        <div>
+        <div className='gifts'>
             <h1>Gifts:</h1>
             {
                 gifts.map(gift => (
