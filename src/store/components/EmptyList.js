347a6816c49840543b22799035984dad07187539
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-08-16 22:34:32 -0500

    Step 6: Adding a message when there are no gifts in the list

---
+++ src/store/components/EmptyList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const EmptyList = () => {
+    return (
+        <div>There is no gifts in your list. Please start adding some!</div>
+    )
+}
--- src/store/components/GiftScreen.js
+++ src/store/components/GiftScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { GiftForm } from './GiftForm';
 import { GiftsList } from './GiftsList';
+import { EmptyList } from './EmptyList';
 
 import { cleanList } from '../slices/gifts';
 
@@ -10,16 +11,25 @@ import './styles.css';
 
 export const GiftScreen = () => {
 
+    const { gifts } = useSelector( state => state.gifts );
     const dispatch = useDispatch();
 
     const handleCleanList = () => {
         dispatch( cleanList() );
     }
 
+    console.log(gifts);
+
     return (
         <div className='gifts'>
             <GiftForm />
-            <GiftsList />
+
+            
+            {
+                gifts.length === 0 
+                    ? (<EmptyList />)
+                    : (<GiftsList />)
+            }            
 
             <button
                 onClick={handleCleanList}
