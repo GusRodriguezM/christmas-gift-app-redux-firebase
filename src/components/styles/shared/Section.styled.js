ad28195ef2ce173646c2440a03b44046f34a18cb
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-09-06 00:38:04 -0500

    Extra: Adding width to the section styled component

--- src/components/styles/shared/Section.styled.js
+++ src/components/styles/shared/Section.styled.js
@@ -4,6 +4,7 @@ export const Section = styled.div`
     display: flex;
     flex-direction: column;
     overflow-wrap: break-word;
+    width: 20%;
 `;
 
 export const SectionPrint = styled(Section)`
