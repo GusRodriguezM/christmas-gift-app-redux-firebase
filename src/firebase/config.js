4ab488c9b9ea116540153babd5a98c1ae3357ddf
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-09-29 03:27:41 -0500

    Adding Firebase and configuring a file to connect it

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0oQ-VGPqsvVbO96jZvLBQD_sEg2OKIO4",
+  authDomain: "todo-app-8a284.firebaseapp.com",
+  projectId: "todo-app-8a284",
+  storageBucket: "todo-app-8a284.appspot.com",
+  messagingSenderId: "1090843493927",
+  appId: "1:1090843493927:web:c7632a314888046e11b002"
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebasDB = getFirestore(FirebaseApp);
