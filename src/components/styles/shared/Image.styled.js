f11cb9cb47b28e9eeedde50e6370685453e8763b
Gustavo RM <gustavo.rm.isc@gmail.com>
2022-10-25 02:53:37 -0500

    Adding breakpoints to the images

--- src/components/styles/shared/Image.styled.js
+++ src/components/styles/shared/Image.styled.js
@@ -3,4 +3,16 @@ import styled from 'styled-components';
 export const Image = styled.img`
     height: 100px;
     width: 100px;
+
+    @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobileMedium}) {
+        height: 60px;
+        width: 60px;
+        border: 1px solid blue;
+    }
+
+    @media screen and (min-width: ${({ theme }) => theme.breakpoints.mobileMediumPlus1}) and (max-width: ${({ theme }) => theme.breakpoints.medium}){
+        height: 80px;
+        width: 80px;
+        border: 1px solid green;
+    }
 `;
